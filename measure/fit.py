6dcda29c8c2bae5e2968eff6a34c99682e6ea184
HuangZhiyuan <2584731599@mail.nwpu.edu.cn>
2025-04-28 03:41:23 +0800

    Separate measurement and fitting

--- measure/fit.py
+++ measure/fit.py
@@ -1,27 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 假设你有一些坐标数据
-x = np.array([0, 50, 100, 150, 200, 250, 300])
-y = np.array([1280, 1240, 1158, 1033, 855, 598, 189])
+result = []
+with open('measure/data.csv', 'r') as f:
+    for line in f:
+        a, b = map(float, line.strip().split(','))
+        result.append([a, b])
 
-for i in range(len(y)):
-    y[i] -= 1280
-    y[i] = -y[i]
+x, y = [], []
+for cnt in result:
+    x.append(cnt[0])
+    y.append(cnt[1])
+
+# print("x数组:", x)
+# print("y数组:", y)
 
 # 拟合一个二次多项式（可以改变拟合阶数）
-coefficients = np.polyfit(x, y, 2)  
+mi = 8
+coefficients = np.polyfit(x, y, mi)
 poly = np.poly1d(coefficients)
 
+expr = ""
+for i, coef in enumerate(coefficients):
+    power = mi - i
+    if power == 0:
+        expr += f"{coef:.6f}"
+    elif power == 1:
+        expr += f"{coef:.6f}x + "
+    else:
+        expr += f"{coef:.6f}x^{power} + "
+expr = expr.rstrip(" + ")
+print(f"拟合的多项式为: {expr}")
+
 # 打印拟合的多项式解析式
-print(f"拟合的二次多项式为: y = {coefficients[0]}x² + {coefficients[1]}x + {coefficients[2]}")
+# print(f"拟合的二次多项式为: y = {coefficients[0]}x² + {coefficients[1]}x + {coefficients[2]}")
+# print(f"拟合的一次多项式为: y = {coefficients[0]}x + {coefficients[1]}")
 
 # 生成拟合曲线
 x_fit = np.linspace(min(x), max(x), 100)
 y_fit = poly(x_fit)
 
 # 绘制结果
-plt.scatter(x, y, color='red', label='Data Points')
-plt.plot(x_fit, y_fit, label='Fitted Curve', color='blue')
+plt.scatter(x, y, color='red', label='Data')
+plt.plot(x_fit, y_fit, label='Fit', color='blue')
+
 plt.legend()
 plt.show()
--- measure/measure.py
+++ measure/measure.py
@@ -8,6 +8,10 @@ import time
 import mss
 import numpy as np
 import cv2
+import shutil
+import matplotlib.pyplot as plt
+
+folder_path = 'measure/src'
 
 print("Push 'S' to start.")
 
@@ -16,19 +20,39 @@ if not os.path.exists('measure/src'):
 
 while True:
     if keyboard.is_pressed('a'):
-        for i in range(0, 301, 50):
-            mouse_control.move_mouse_relative(i, 0)
+        if os.path.exists(folder_path):
+            shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+
+        result = []
+
+        step = 1
+        for i in range(0, 1001, step):
+            mouse_control.move_mouse_relative(step, 0)
             with mss.mss() as sct:
                 monitor = sct.monitors[0]
                 img = np.array(sct.grab(monitor))
             frame = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
             mask = vision.preprocess(frame)
             targets = vision.find_targets(mask)
+            L_cx,L_cy,L_r = 100000,0,0
             for cx, cy, radius in targets:
-                cv2.circle(frame, (int(cx), int(cy)), 5, (0, 0, 255), -1)
-                text = f"({int(cx)}, {int(cy)}, {int(radius)})"
-                cv2.putText(frame, text, (int(cx)+10, int(cy)-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                print(f"x: {cx} y: {cy} radius: {radius}")
-            cv2.imwrite(f"measure/src/{i}.jpg", frame)
-            print('\n')
-            time.sleep(0.3)
+                if cx < L_cx:
+                    L_cx,L_cy,L_r = cx,cy,radius
+            cv2.circle(frame, (int(L_cx), int(L_cy)), 5, (0, 0, 255), -1)
+            text = f"({int(L_cx)}, {int(L_cy)}, {int(L_r)})"
+            cv2.putText(frame, text, (int(L_cx)+10, int(L_cy)-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            # print(f"x: {L_cx} y: {L_cy} radius: {L_r}")
+            result.append([i,L_cx])
+            if i % 100 == 0: cv2.imwrite(f"measure/src/{i}.jpg", frame)
+        
+        for i in range(1,len(result)):
+            result[i][1] = result[i][1] - result[0][1]
+            result[i][1] = -result[i][1]
+        result[0][1] = 0
+
+        with open('measure/data.csv', 'w') as f:
+            for cnt in result:
+                f.write(f"{cnt[0]},{cnt[1]}\n")
+
+        break
